e061a5880914bc002e25e9e37ca50484193cc6b7
Vijay Singh <vijaysoophie@gmail.com>
2020-11-24 13:45:30 -0500

    updated label instance to calcualte motion angle

--- labelInstances.m
+++ labelInstances.m
@@ -1,4 +1,4 @@
-function nIdentifiedInstances = labelInstances(trackMatrix, proximityRadius)
+function videoInfo = labelInstances(trackMatrix, proximityRadius)
 %function labeledTrackMatrix = labelInstances(trackMatrix)
 %
 % Example call labeledTrackMatrix = labelInstances(trackMatrix)
@@ -10,33 +10,59 @@ function nIdentifiedInstances = labelInstances(trackMatrix, proximityRadius)
 %   proximityRadius: The maximum distance at which the cell can be in next frame
 %
 % Output:
-%  labeledTrackMatrix: track matrix with the correct labels
+%  videoInfo: struct with the following fields
+%           NIdentifiedInstances: Number of instances in frame
+%           instanceIndex: Index of the instance in trackMatrix
+%           positions: position of instances 
+%           headAngle: angle of head-body line wrt to x-axis 
+%           labels: Instance labels
+%           motionAngle: angle of the head positions in two consecutive
+%                   frames with respect to x-axis
+%
 % 
 % Vijay Singh wrote this Nov 21 2020
+% Vijay Singh modified this Nov 24 2020
 %
 %%
-% First find out the number of instances on each frame
-nInstance = size(trackMatrix,4);            % Number of possible instances
-nFrames = size(trackMatrix,1);              % Numebr of frames in video
-nIdentifiedInstances = zeros(nFrames,nInstance); % Number of instances on the given frame
+videoInfo = struct();
+totalFrames = size(trackMatrix, 1);    % The number of frames in the video.
 
-for iterFrames = 1:nFrames
-    for iterInstance = 1:nInstance
-        if ~isnan(max(max(squeeze(trackMatrix(iterFrames,:,:,iterInstance)))))
-            nIdentifiedInstances(iterFrames,iterInstance) = 1;
-        end
-    end
+for iterFrames = 1: totalFrames
+    frameInfo = getFrameInfo(trackMatrix, iterFrames);
+    videoInfo.NIdentifiedInstances{iterFrames} = frameInfo.NIdentifiedInstances;
+    videoInfo.instanceIndex{iterFrames} = frameInfo.instanceIndex;
+    videoInfo.positions{iterFrames} = frameInfo.positions;
+    videoInfo.headAngle{iterFrames} = frameInfo.headAngle;
 end
 
-labeledTrackMatrix = trackMatrix;   % intialization
+videoInfo.labels{1} = [1:videoInfo.NIdentifiedInstances{1}];
+videoInfo.motionAngle{1} = NaN(1,videoInfo.NIdentifiedInstances{1});
 
-labeledTrackMatrix = labeledTrackMatrix(:,:,:, ...
-            [find(nIdentifiedInstances(1,:)) find(~nIdentifiedInstances(1,:))]);
+for iterFrames = 2:totalFrames
+    % For every identified instance in the last frame, find the nearest
+    % neighbour in the current frame. If it neighbour lies in the proximity
+    % radius, call it the same label otherwise say there is no neighbour.
+    % After all instances from previous frame are labeled, label the rest
+    % of the instances in the current frame.
+    NIdentifiedInstances = videoInfo.NIdentifiedInstances{iterFrames};
+    positionsThisFrame = videoInfo.positions{iterFrames};
+    positionsLastFrame = videoInfo.positions{iterFrames-1};
+    videoInfo.labels{iterFrames} = zeros(1,videoInfo.NIdentifiedInstances{iterFrames});
+    videoInfo.motionAngle{iterFrames} = NaN(1,videoInfo.NIdentifiedInstances{iterFrames});
 
-for iterFrames = 2:nFrames
-    % compare the number of instances in this frame as compared the last 
-    % frame. 
-    nIdentifiedInstances(iterFrames) 
+    for iterInstances = 1 : NIdentifiedInstances
+        distances = sum((squeeze(positionsLastFrame(1,:,:)) - ...
+                            squeeze(positionsThisFrame(1,:,iterInstances))').^2);
+        [minDistance, minPosition] = min(distances);
+        
+        if minDistance < proximityRadius^2
+            videoInfo.labels{iterFrames}(iterInstances) = videoInfo.labels{iterFrames-1}(minPosition);
+            zz = squeeze(videoInfo.positions{iterFrames}(1,:,iterInstances) - videoInfo.positions{iterFrames-1}(1,:,minPosition));
+            videoInfo.motionAngle{iterFrames}(iterInstances) = angle(zz(1)+1i*zz(2));
+        end
+    end
+    unlabeledInstances = find(videoInfo.labels{iterFrames}==0);
+    videoInfo.labels{iterFrames}(unlabeledInstances) = max(videoInfo.labels{iterFrames-1}) + [1:length(unlabeledInstances)];
 end
     
 
---
+++ testLabelInstances.m
@@ -0,0 +1,53 @@
+function testLabelInstances(videoInfo, width, height)
+%function NIdentifiedInstances = plotPositionsByFrame(trackMatrix, frameNumber, frameWidth, frameHeight)
+%
+% Example call NIdentifiedInstances = plotPositionsByFrame(tracks_matrix, 1, width, height);
+%
+% This function plots of the position of each instance on the frameNumber
+% frame from the data provided in the matrix trackMatrix. The function also
+% counts the actual number of instances identified in the frame. The first
+% node on the instance (head) is plotted as a big dot. The rest of the
+% nodes are plotted using connected line segments.
+%
+% INPUT:
+%   videoInfo: Struct with fields. Each field is a cell.
+%               NIdentifiedInstances: Number of instances in frame
+%               instanceIndex: Index of the instance in trackMatrix
+%               positions: position of instances 
+%               headAngle: angle of head-body line wrt to x-axis 
+%               labels: Instance labels
+%
+% Output:
+%  NIdentifiedInstances: Number of identified instances; Integer
+% 
+% Vijay Singh wrote this Nov 20 2020
+%
+%% Test the positions and angles
+refreshFigure = 0;
+figure; hold on;
+for iterFrames = 1:size(videoInfo.NIdentifiedInstances,2)
+    NIdentifiedInstances = videoInfo.NIdentifiedInstances{iterFrames};
+    instanceIndex = videoInfo.instanceIndex{iterFrames};
+    positions = videoInfo.positions{iterFrames};
+    headAngle = videoInfo.headAngle{iterFrames};
+    labels = videoInfo.labels{iterFrames};
+    for ii = 1:NIdentifiedInstances
+%         plot(squeeze(positions(:,1,ii)), ...
+%             squeeze(positions(:,2,ii)),'.--', 'MarkerSize',20, 'LineWidth',2);
+%         plot([squeeze(positions(2,1,ii)) squeeze(positions(2,1,ii))+10*cos(headAngle(ii))], ...
+%             [squeeze(positions(2,2,ii)) squeeze(positions(2,2,ii))+10*sin(headAngle(ii))],'-', 'LineWidth',2);
+        plot([squeeze(positions(1,1,ii))], ...
+            [squeeze(positions(1,2,ii)) ],'.', 'MarkerSize',10);
+        text(squeeze(positions(1,1,ii))+10,squeeze(positions(1,2,ii))+10, num2str(labels(ii)));
+    end
+    refreshFigure = refreshFigure + 1;
+    if mod(refreshFigure,5) == 0
+        pause();
+        clf; hold on;
+        xlim([1, width]);
+        ylim([1, height]);
+    end
+end
+
+
+
